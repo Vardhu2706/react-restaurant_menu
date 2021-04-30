b981a41b453907fd9c38741e9ff18d8a65548676
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-04-30 11:46:48 +0530

    Inital Project setup.

---
+++ src/Categories.js
@@ -0,0 +1,12 @@
+// Importing Helpers
+import React from "react";
+
+// Importing Components
+
+// Functional Components
+const Categories = () => {
+  return <h2>Categories Component</h2>;
+};
+
+// Default Export
+export default Categories;
---
+++ src/Menu.js
@@ -0,0 +1,12 @@
+// Importing Helpers
+import React from "react";
+
+// Import Components/ Files
+
+// Functional Component
+const Menu = () => {
+  return <h2>Menu Component</h2>;
+};
+
+// Default Export
+export default Menu;
